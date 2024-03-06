9fa72f26863d479ee31b5366f58769826c5fa63f
Philipp Bolte <philipp.bolte@spherity.com>
2024-03-06 14:04:58 +0100

    chore: little restructure to increase readability

--- hooks/useEvents.ts
+++ hooks/useEvents.ts
@@ -1,6 +1,6 @@
 import {useCallback, useEffect, useState} from 'react';
-import {Address, createPublicClient, custom, http, parseAbiItem} from "viem";
-import {mainnet, sepolia, usePublicClient} from "wagmi";
+import {Address, parseAbiItem} from "viem";
+import {usePublicClient} from "wagmi";
 import {PRD_REGISTRY_ADDRESS} from "../lib/utils";
 
 export type HintPath = {
@@ -11,7 +11,25 @@ export type HintPath = {
 }
 
 const DELETE_FLAG = "0x0000000000000000000000000000000000000000000000000000000000000000"
+const HINT_VALUE_CHANGED_ABI_ITEM = parseAbiItem('event HintValueChanged(address indexed namespace, bytes32 indexed list, bytes32 indexed key, bytes32 value)');
 
+// Function to filter out removed hints from the events array
+const filterRemovedHints = (events: HintPath[]) => {
+  const seenHints: Record<string, HintPath> = {};
+
+  for (const event of events) {
+    const { namespace, list, key, value } = event;
+    const hintPath = `${namespace}_${list}_${key}`; // Construct a unique path for the hint
+
+    if (value !== DELETE_FLAG) {
+      seenHints[hintPath] = event; // Add the hint to the seenHints object
+    } else {
+      delete seenHints[hintPath]; // Remove the hint from the seenHints object
+    }
+  }
+
+  return Object.values(seenHints); // Return an array of the remaining hints
+};
 /*
  * Returns all Hint events for a given namespace for all Hints that are not removed
  */
@@ -29,35 +47,25 @@ function useHintEvents({namespace, registryAddress}: {namespace: Address, regist
     setIsError(false);
     try {
       // Get all events for the given namespace where a hint was added or updated
-      const events = await client.getLogs({
+      const logs = await client.getLogs({
         address: registryAddress,
-        event: parseAbiItem('event HintValueChanged(address indexed namespace, bytes32 indexed list, bytes32 indexed key, bytes32 value)'),
+        event: HINT_VALUE_CHANGED_ABI_ITEM,
         args: {
           namespace,
         },
         fromBlock: 'earliest'
       })
 
-      // Filter out removal events where a hint was updated to 0x0... (delete flag)
-      const seenObjects: Record<string, HintPath> = {};
-      for (let i = 0; i <= events.length - 1; i++) {
-        const event = events[i];
-        const { namespace, list, key, value } = event.args;
-        const path = `${namespace}_${list}_${key}`;
+      // Map the logs to HintPath objects
+      const events = logs.map((log) => ({
+        namespace: log.args.namespace as `0x${string}`,
+        list: log.args.list as `0x${string}`,
+        key: log.args.key as `0x${string}`,
+        value: log.args.value as `0x${string}`,
+      }));
 
-        // When deleting a hint/ trusted issuer value, it gets overwritten with 0x0...
-        if (!seenObjects[path] && value != DELETE_FLAG) {
-          seenObjects[path] = {
-            namespace: event.args.namespace ?? '0x0',
-            list: event.args.list ?? '0x0',
-            key: event.args.key ?? '0x0',
-            value: event.args.value ?? '0x0',
-          };
-        } else if (value == DELETE_FLAG) {
-          delete seenObjects[path];
-        }
-      }
-      setEvents(Object.values(seenObjects) as any)
+      const filteredEvents = filterRemovedHints(events);
+      setEvents(filteredEvents);
     } catch (e) {
       setIsError(true);
     } finally {
@@ -68,7 +76,10 @@ function useHintEvents({namespace, registryAddress}: {namespace: Address, regist
   // useEffect will run the refetch function on initial render and whenever the refetch function changes
   // the refetch function changes when their dependencies change which triggers a call of refetch itself via useEffect
   useEffect(() => {
-    refetch();
+    const fetchData = async () => {
+      await refetch();
+    };
+    fetchData();
   }, [refetch]);
 
   // return the events, loading state, error state and the refetch function for external triggering
